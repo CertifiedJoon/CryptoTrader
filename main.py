57ed9bad9f4f613d4c012786a1535d5ac0b7e4f5
CertifiedJoon <u3566665@connect.hku.hk>
2021-05-16 04:03:09 +0000

    created rough structure for trading will try to buy and sell as testing

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
-import jumbo as cj
+import Trader
 import ccxt
 import datetime
 import time
+import ccxt 
 
-binance = ccxt.binance()
-df = cj.get_df(binance, "BTC/USDT")
-
-now = datetime.datetime.now()
-mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
-
-target_price = cj.get_target(df)
+bitcoin = Trader('BTC/USDT', 'BTCUSDT.csv')
 
 while True:
-    now = datetime.datetime.now()
-    if mid < now < mid + datetime.delta(seconds=10):
-        target_price = cj.get_target(df)
-        mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
-        
-    current_price = cj.get_price(binance, "BTC/USDT")
-    print(current_price, target_price)
-    
-    time.sleep(1)
+    try:
+        now = datetime.datetime.now()
+        start_time = datetime.datetime.utcnow().strftime('%Y-%m-%d 00:00:00')
+        end_time = start_time + datetime.timedelta(days=1)
+        if start_time < now < end_time - datetime.timedelta(seconds=10):
+            target_price = bitcoin.get_target()
+            current_price = bitcoin.get_price()
+            if target_price < current_price:
+                usdt = bitcoin.fetch_balance()['free']
+                if usdt > 10:
+                    upbit.buy_market_order("KRW-BTC", krw*0.9995)
+        else:
+            btc = get_balance("BTC")
+            if btc > 0.00008:
+                upbit.sell_market_order("KRW-BTC", btc*0.9995)
+        time.sleep(1)
+    except Exception as e:
+        with open('error.txt', 'a') as f:
+            f.write(e)
+        time.sleep(1)
