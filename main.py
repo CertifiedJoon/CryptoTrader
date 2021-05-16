f8b63959887ab87dfacc887945f73bcfe826cd10
CertifiedJoon <u3566665@connect.hku.hk>
2021-05-16 06:21:15 +0000

    debugged main trading process. Works like a charm

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
-import Trader
+from Trader import Trader
 import ccxt
 import datetime
 import time
 import ccxt 
-
+import pprint
 bitcoin = Trader('BTC/USDT', 'BTCUSDT.csv')
-
+pprint.pprint(bitcoin.get_balance())
 while True:
     try:
+        print(bitcoin.get_price())
         now = datetime.datetime.now()
-        start_time = datetime.datetime.utcnow().strftime('%Y-%m-%d 00:00:00')
+        start_time = datetime.datetime.now()
+        start_time -= datetime.timedelta(hours = start_time.hour, minutes = start_time.minute, seconds = start_time.second, microseconds = start_time.microsecond)
         end_time = start_time + datetime.timedelta(days=1)
+        target_price = bitcoin.get_target()
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = bitcoin.get_target()
+            print('target {}'.format(target_price))
             current_price = bitcoin.get_price()
             if target_price < current_price:
-                usdt = bitcoin.fetch_balance()['free']
-                if usdt > 10:
-                    upbit.buy_market_order("KRW-BTC", krw*0.9995)
+                usdt = bitcoin.get_balance()
+                if usdt['free'] > 10:
+                    print('bought')
         else:
             btc = get_balance("BTC")
             if btc > 0.00008:
-                upbit.sell_market_order("KRW-BTC", btc*0.9995)
+                print('sold')
         time.sleep(1)
     except Exception as e:
         with open('error.txt', 'a') as f:
-            f.write(e)
+            f.write(str(e))
         time.sleep(1)
