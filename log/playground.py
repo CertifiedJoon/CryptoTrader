cf1992dbf83f63971bc0dc4ff2cf7efa2e80be6d
CertifiedJoon <u3566665@connect.hku.hk>
2021-05-18 10:14:32 +0000

    minor restructuring

--- log/playground.py
+++ log/playground.py
@@ -1,22 +1,66 @@
-import ccxt
-import pprint
-import datetime
+#Description: This program gets the sentiment of Bitcoin from Twitter
+import tweepy
+from textblob import TextBlob
+import pandas as pd
+import numpy as np
+import re
 
-with open('api.txt') as f:
-    lines = f.readlines()
-    api_key = lines[0].strip()
-    secret = lines[1].strip()
-    
-binance = ccxt.binance(config={
-    'apiKey' : api_key,
-    'secret' : secret
-})
+CONSUMER_KEY= 'thtCNwOql2tuWs6EvSAUUTBXg'
+CONSUMER_KEY_SECRET = 'WNxdctkNdkpIyVQQGTY82CZBRZZNaBoviZwO9n5fEEP6GiUdG6'
 
-balance = binance.fetch_balance()
-pprint.pprint(balance)
+ACCESS_TOKEN = '1394225479775756289-cFGjGk0sE7DxtjWrsmKujlApPmzjqM'
+ACCESS_TOKEN_SECRET = 'Q3U2AK2b5vVsthJX2edm2ugQJrPa5wERS9L7n3G6Cy6zo'
 
-start_time = datetime.datetime.now()
-start_time -= datetime.timedelta(hours = start_time.hour, minutes = start_time.minute, seconds = start_time.second, microseconds = start_time.microsecond)
-end_time = start_time + datetime.timedelta(days=1)
-now = datetime.datetime.utcnow()
-print(start_time, now ,end_time)
+print("authenticating tweetpy")
+#create authentication object
+authenticate = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_KEY_SECRET)
+
+print("set access")
+#Set the access tokens
+authenticate.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+#Create the API object
+print("creating api")
+api = tweepy.API(authenticate, wait_on_rate_limit=True)
+
+print("gathering tweets")
+#Gather 2000 tweets about Bitcoin and filer out any retweets 'RT'
+search_term = '#bitcoin -filter:retweets'
+
+print("searching cursor")
+#Create a cursor object
+tweets = tweepy.Cursor(api.search, q=search_term, lang='en', since='2021-05-10', tweet_mode='extended').items(2000)
+#Store the tweets in a variable and get the full text
+all_tweets = [tweet.full_text for tweet in tweets]
+
+print("creating dataframe")
+#Create a dataframe to store the tweets with a column called 'Tweets'
+df = pd.DataFrame(all_tweets, columns=['Tweets'])
+
+#Create a function to clean the tweets
+def cleanTwt(twt):
+  twt = re.sub('#bitcoin', 'bitcoin', twt) #removes the hashtag from #bitcoin
+  twt = re.sub('#Bitcoin', 'Bitcoin', twt) #removes the hashtag from #Bitcoin
+  twt = re.sub('#[A-Za-z0-9]+', '', twt) #removes anything with hashtag
+  twt = re.sub('@[A-Za-z0-9]+', '', twt)
+  twt = re.sub('\\n','',twt) #removes newlines
+  twt = re.sub('https?:\/\/\S+', '', twt) #removes any hyperlinks
+  return twt
+
+
+print("clearning tweets")
+#Clean tweets gathered
+df['CleanTweets'] = df['Tweets'].apply(cleanTwt)
+#Create a function to get the subjectivity
+def get_subjectivity(twt):
+  return TextBlob(twt).sentiment.subjectivity
+
+#Create a function to get the polarity
+def get_polarity(twt):
+  return TextBlob(twt).sentiment.polarity
+
+print("get subjectivity")
+#Create two new columns called 'subjectivity' and 'polarity'
+df['Subjectivity'] = df['CleanTweets'].apply(get_subjectivity) 
+df['Polarity'] = df['CleanTweets'].apply(get_polarity)
+
+print(pd.mean(df['Polarity']))
