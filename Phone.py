42ed1201237b30043049a9eaaf0c6a922c2474cc
CertifiedJoon <u3566665@connect.hku.hk>
2021-05-31 14:31:18 +0000

    added gitignore

--- Phone.py
+++ Phone.py
@@ -2,8 +2,9 @@ import requests
 
 def post_message(text):
     """슬랙 메시지 전송"""
-    
-    token = "xoxb-2090282742643-2090295046947-Vl7EZ13JeEKoEAVQQPNGTFzs"
+    with open("credentials/slack.txt") as f:
+        lines = f.readlines()
+        token = lines[0].strip()
     response = requests.post("https://slack.com/api/chat.postMessage",
         headers={"Authorization": "Bearer "+token},
         data={"channel": '#personalproject',"text": text}
